06d0322e838a09a186ffd611caf27118946f50b9
jpmassari <joao.massari1997@gmail.com>
2023-12-05 14:47:39 -0300

    goto

--- goto.ts
+++ goto.ts
@@ -4,27 +4,28 @@ export const goto_block = (blocks: [string, () => void][]): void => {
     const goto_block_impl = (name?: string): void => {
         try {
             const exec = (blocks: [string, () => void][]): void => {
-                blocks.map((block) => block[1]())
+                let i = 0;
+                while (i < blocks.length) {
+                    blocks[i]![1]();
+                    i = i + 1;
+                } 
             }
             const skip = (blocks: [string, () => void][], entry: string) => {
-                switch (blocks[0]![0]) {
-                    case undefined: return;
-                    case blocks[0]![0]: {
-                        if(blocks[0]![0] === entry) {
-                            exec(blocks)
-                        } else {
-                            skip(blocks.slice(1), entry)
-                        }
+                    if (blocks.length === 0) return;
+                    if (blocks[0]![0] === entry) exec(blocks);
+                    else {
+                        blocks.shift();
+                        skip(blocks, entry);                 
                     }
-                }
             }
-        switch (name) {
-            case undefined: exec(blocks); break;
-            case name!: skip(blocks, name); break;
-        }
+            switch(name) {
+                case undefined: exec(blocks); break;
+                case name!: skip(blocks, name); break;
+            }
         } catch(e: any) {
-            goto_block_impl(e)
+            goto_block_impl(e);
         }
     }
-    goto_block_impl()
-}
+
+    goto_block_impl();
+};
